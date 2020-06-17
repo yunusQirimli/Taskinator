123a6e7f66aaf612301c05ee1859db6bce3b4273
Yunus Yunusov <yunus1699@gmail.com>
2020-06-17 13:12:36 +0200

    change path to app logo

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -101,7 +101,7 @@ module.exports = (options) => webpackMerge(commonConfig({ env: ENV }), {
     ]),
     new WebpackNotifierPlugin({
       title: 'JHipster',
-      contentImage: path.join(__dirname, 'logo-jhipster.png')
+      contentImage: path.join(__dirname, 'logo-taskinator.png')
     })
   ].filter(Boolean)
 });
